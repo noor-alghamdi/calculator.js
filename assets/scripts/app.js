20cffca3a098761c7352a1649dd346987dad5a23
noor-alghamdi <namacoconut@gmail.com>
2023-05-08 13:41:41 +0300

    sum operation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,11 +5,41 @@ function getUserInput(){
     return parseInt(userInput.value)
 }
 
-function add() {
-    const enteredNumber =  getUserInput()
-    const printOperation = currentResult + '+' + enteredNumber 
-    currentResult = currentResult + enteredNumber 
-    outputResult(currentResult, printOperation)
+function createWriteOutput(operator, resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription)
 }
 
+
+function add() {
+    const enteredNumber =  getUserInput()
+    const intialResult = currentResult
+    currentResult = currentResult + enteredNumber
+    createWriteOutput('+', intialResult, enteredNumber)
+}
 addBtn.addEventListener('click', add)
+
+function subtract(){
+    const enteredNumber =  getUserInput()
+    const intialResult = currentResult
+    currentResult = currentResult - enteredNumber
+    createWriteOutput('-', intialResult, enteredNumber)
+}
+subtractBtn.addEventListener('click', subtract)
+
+function multiply(){
+    const enteredNumber =  getUserInput()
+    const intialResult = currentResult
+    currentResult = currentResult * enteredNumber
+    createWriteOutput('*', intialResult, enteredNumber)
+}
+multiplyBtn.addEventListener('click', multiply)
+
+
+function devide(){
+    const enteredNumber =  getUserInput()
+    const intialResult = currentResult
+    currentResult = currentResult / enteredNumber
+    createWriteOutput('/', intialResult, enteredNumber)
+}
+divideBtn.addEventListener('click', devide)
